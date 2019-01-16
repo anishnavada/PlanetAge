100a9e9dcc1ab713eb7d9f579cd22fa1170c4ff3
[anishnavada] <navadaa1@bxscience.edu>
2019-01-16 11:35:58 -0500

    Add Project

---
+++ app/src/main/java/com/example/anishnavada/planetweight/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.anishnavada.planetweight
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Mercury.setOnClickListener {
+            changeAge(87.97)
+        }
+        Mars.setOnClickListener {
+            changeAge(686.2)
+        }
+        Venus.setOnClickListener {
+            changeAge(224.7)
+        }
+        Uranus.setOnClickListener {
+            changeAge(30663.65)
+        }
+        Jupiter.setOnClickListener {
+            changeAge(4328.9)
+        }
+        Saturn.setOnClickListener {
+            changeAge(10752.9)
+        }
+    }
+
+   fun changeAge(factor: Double){
+        var days_old =  (years.text.toString().toDouble() * 365.0) + (months.text.toString().toDouble() * 30.0) + (days.text.toString().toDouble())
+        var result_years = days_old/factor
+        result.text = result_years.toString()
+    }
+}
